a9de97af9cc71f2f0d05cfaca4fa85c6357e4011
Bonafede Nicolas <n.bonafede@student.helmo.be>
2026-02-16 11:25:40 +0000

    Remplacer main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from src.generation.generateur import generer_reponse
+from src.search.recherche import rechercher_passages
 
-question = "Qui est le roi ?"
-passages = ["Le roi Aldric règne depuis 40 ans sur les Terres du Nord."]
+question = "Qui est le souverain incontesté du Royaume Humain d'Aethelgard depuis le lancement du jeu il y a 10 ans. ?"
 
-reponse = generer_reponse(question, passages)
+passages, _question_retournee = rechercher_passages(question)
 
-print(reponse)
+if not passages:
+    print("Aucun passage pertinent trouve.")
+else:
+    reponse = generer_reponse(question, passages)
+    print(reponse)
