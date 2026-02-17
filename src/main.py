ad73ce18bbcc6be7083ca4ced755d2655fbc81c9
Makhtsaev Emir <e.makhtsaev@student.helmo.be>
2026-02-17 13:54:24 +0000

    Supprimer main.py

--- src/main.py
+++
@@ -1,8 +0,0 @@
-from src.generation.generateur import generer_reponse
-
-question = "Qui est le roi ?"
-passages = ["Le roi Aldric règne depuis 40 ans sur les Terres du Nord."]
-
-reponse = generer_reponse(question, passages)
-
-print(reponse)
